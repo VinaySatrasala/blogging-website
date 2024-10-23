a5f6aa2848f93900ff4b0ac193a42675bec98b37
vinay satrasala <satrasalavinaykumar.01@gmail.com>
2024-10-24 00:10:12 +0530

    bootstrapped application and implemented signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,51 @@
 import { Hono } from 'hono'
+import { Prisma, PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+type bindings = {
+  DATABASE_URL :string
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{Bindings:bindings}>();
 
 
-app.post("/api/v1/signup",(c)=>{
-  return c.text("signup")
+// app.get('/', (c) => {
+//   const prisma = new PrismaClient({
+//     datasourceUrl : c.env.DATABASE_URL
+//   }).$extends(withAccelerate());
+//   return c.text('Hello Hono!')
+// })
+
+
+app.post("/api/v1/signup",async (c)=>{
+
+  // Prisma client
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  // Getting body from req
+  const body = await c.req.json();
+
+  // Pushing it to DB
+  let uid:any;
+  try{
+    uid = await prisma.user.create({
+      data:{
+        email : body.email,
+        name : body.name,
+        password : body.password,
+      },
+      select:{
+        id:true
+      }
+    })
+  }catch(error:any){
+    console.log(error.message);
+    return c.json({msg : error.message});
+  }
+
+  return c.json(`User created succesfully with UID: ${uid.id}`);
 })
 
 app.post("/api/vi/signin",(c)=>{
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 Simple Medium kind blogging website
+
+
+npm install prisma @prisma/client@latest @prisma/extension-accelerate
+
+npx prisma generate --no-engine
